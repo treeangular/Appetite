6d154284599a790f915b7205bb7d99c03f6692e7
Javito Hertfy <javierhertfelder@gmail.com>
2014-08-31 09:34:50 +0200

    Starting to add demo module

---
+++ www/demo/demo.module.js
@@ -0,0 +1,9 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Javito
+ * Date: 31/08/14
+ * Time: 9:26
+ * To change this template use File | Settings | File Templates.
+ */
+angular.module('treeangular.demo');
+
