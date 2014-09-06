98aca84350e7bf2b3c71c4e60d506586496ae10b
Javito Hertfy <javierhertfelder@gmail.com>
2014-09-06 11:53:51 +0200

    asdasdas

---
+++ www/modules/demo/bootstrap.js
@@ -0,0 +1,8 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Javito
+ * Date: 3/09/14
+ * Time: 20:18
+ * To change this template use File | Settings | File Templates.
+ */
+angular.module('demoModule', []);
