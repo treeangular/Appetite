77a445745bf4ee88cb7fda070aba97355d1c5bb9
Javito Hertfy <javierhertfelder@gmail.com>
2014-09-06 11:54:09 +0200

    Revert "asdasdas"
    This reverts commit 3f8862928a14d31e807023971ab6824542b4df49.

--- www/modules/demo/bootstrap.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created with JetBrains WebStorm.
- * User: Javito
- * Date: 3/09/14
- * Time: 20:18
- * To change this template use File | Settings | File Templates.
- */
-angular.module('demoModule', []);
