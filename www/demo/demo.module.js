6c07c2dd2432d93622f4459d9c8a093556093e72
Javito Hertfy <javierhertfelder@gmail.com>
2014-09-06 12:41:01 +0200

    removed controllers

--- www/demo/demo.module.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created with JetBrains WebStorm.
- * User: Javito
- * Date: 31/08/14
- * Time: 9:26
- * To change this template use File | Settings | File Templates.
- */
-angular.module('treeangular.demo');
-
